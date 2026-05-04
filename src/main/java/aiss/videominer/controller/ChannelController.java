a818898f42354e6e7137a4039cf30a89d385a4f7
AlbertoCV03 <albcamvas@alum.us.es>
2026-05-04 12:45:24 +0200

    ChannelController added httpStatusCreated

--- src/main/java/aiss/videominer/controller/ChannelController.java
+++ src/main/java/aiss/videominer/controller/ChannelController.java
@@ -4,10 +4,8 @@ package aiss.videominer.controller;
 import aiss.videominer.model.Channel;
 import aiss.videominer.repository.ChannelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/channels")
@@ -15,6 +13,7 @@ public class ChannelController {
     @Autowired
     ChannelRepository channelRepository;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void addChannel(@RequestBody Channel channel) {
         channelRepository.save(channel);
